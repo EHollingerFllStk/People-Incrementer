341ebe30eaedd67cc9cd05c082f2b03782e5aa74
Beth-Hollinger <EHollingerFullStackEngineer@gmail.com>
2023-01-13 13:45:54 -0500

    add button and styling, js practice

---
+++ script.js
@@ -0,0 +1,21 @@
+//document.getElementById("count-el").innertext = 5;
+
+// let count = 5;
+// count = 3;
+// count = count + 2;
+// console.log(count)
+//JS runs from top down
+
+//Create a variable, bonusPoints. Initialize it as 50. Increase it to 100. 
+//Decrease it down to 25, and then finally increase it to 70
+//console.log the value after each step
+
+let bonusPoints = 50;
+console.log(bonusPoints)
+bonusPoints = bonusPoints + 50;
+console.log(bonusPoints)
+bonusPoints = bonusPoints - 75;
+console.log(bonusPoints)
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints)
+
